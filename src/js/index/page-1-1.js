6bbe47e5629dc54fdda00b172ce04312f3fae708
xlk <1148038404@qq.com>
2018-08-22 22:09:10 +0800

    获取歌曲列表

---
+++ src/js/index/page-1-1.js
@@ -0,0 +1,14 @@
+{
+    let view = {
+        el:'.playlists'
+    }
+    let model={}
+    let controller = {
+        init(view,model){
+            this.view = view
+            this.model = model
+        }
+
+    }
+    controller.init(view,model)
+}
--- src/js/index/page-1.js
+++ src/js/index/page-1.js
@@ -19,6 +19,8 @@
             this.model = model
             this.bindEvent()
             this.bindEventHub()
+            this.loadModule1()
+            this.loadModule2()
         },
         bindEvent(){},
         bindEventHub(){
@@ -29,6 +31,23 @@
                     this.view.hidden()
                 }
             })
+        },
+        loadModule1(){
+            let script1 = document.createElement('script')
+            script1.src = './js/index/page-1-1.js'
+           
+            script1.onload= function(){
+                console.log('模块1加载成功')
+            }
+            document.body.appendChild(script1)
+        },
+        loadModule2(){
+            let script2 = document.createElement('script')
+            script2.src = './js/index/page-1-2.js'
+            script2.onload=function(){
+                console.log('模块2加载成功')
+            }
+            document.body.appendChild(script2)
         }
     }
     controller.init(view,model)
